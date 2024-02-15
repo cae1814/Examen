ddaf02fd4432963e398d347d776bb93f15a97d67
Antonio Banegas <cae1814@gmail.com>
2024-02-14 19:23:06 -0600

    Inicio
    Inicio de Examen

---
+++ ConsoleApp1/Alumno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal abstract class Alumno
+    {
+        public string Nombre { get; set; }
+        public string NumeroCuenta { get; set; }
+        public string Email { get; set; }
+
+        public abstract void Imprimir();
+    }
+}
---
+++ ConsoleApp1/Asignatura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class Asignatura : Alumno
+    {
+        public string Nombre { get; set; }
+        public string Horario { get; set; }
+        public string Docente { get; set; }
+
+        public override void Imprimir() {
+            Console.WriteLine("Nombre: "+this.Nombre);
+            Console.WriteLine("Cuenta: "+ NumeroCuenta);
+            Console.WriteLine("Correo: "+ Email);
+            Console.WriteLine("Asignatura: " + Nombre);
+            Console.WriteLine("Horario: " + Horario);
+            Console.WriteLine("Docente: " + Docente);
+        }
+    }
+}
